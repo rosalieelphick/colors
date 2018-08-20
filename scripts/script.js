457090467de050ad85196d0845296c1d9ad9304b
rosalieelphick <rosalie.elphick@gmail.com>
2018-08-19 20:53:13 -0400

    final palettes added

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,6 @@
 $(function () {
 
     const palettes = [
-        // pastel palettes
         {
             pastel: true,
             cool: true,
@@ -288,19 +287,319 @@ $(function () {
                 "#748997"
             ]
         },
+        {
+            dark: true,
+            warm: true,
+            colors: [
+                "#160e46",
+                "#5f0f78",
+                "#9a165e",
+                "#c71f2d",
+                "#748997"
+            ]
+        },
+        {
+            bright: true,
+            cool: true,
+            colors: [
+                "#d0d3c5",
+                "#59b0bc",
+                "#126f87",
+                "#d83a36",
+                "#062a30"
+            ]
+        },
+        {
+            bright: true,
+            cool: true,
+            colors: [
+                "#476674",
+                "#98fffb",
+                "#f9c8c0",
+                "#6b8d9c",
+                "#cccccc"
+            ]
+        },
+        {
+            bright: true,
+            cool: true,
+            colors: [
+                "#cccccc",
+                "#5370c0",
+                "#b7e8ca",
+                "#9d92ce",
+                "#c67bd0"
+            ]
+        },
+        {
+            bright: true,
+            warm: true,
+            colors: [
+                "#72cbdb",
+                "#55134e",
+                "#a0596b",
+                "#fec343",
+                "#ef7351"
+            ]
+        },
+        {
+            bright: true,
+            warm: true,
+            colors: [
+                "#feefd2",
+                "#ff8c7d",
+                "#ff634d",
+                "#079dab",
+                "#9efff6"
+            ]
+        },
+        {
+            bright: true,
+            warm: true,
+            colors: [
+                "#fcc44d",
+                "#ffaf9f",
+                "#b283a3",
+                "#8baae1",
+                "#ffe2b6"
+            ]
+        },
+        {
+            bright: true,
+            neutral: true,
+            colors: [
+                "#8dcdc1",
+                "#7c786a",
+                "#d3e397",
+                "#eb6e44",
+                "#fff5c3"
+            ]
+        },
+        {
+            bright: true,
+            neutral: true,
+            colors: [
+                "#c6d1da",
+                "#2995b6",
+                "#f8d6a9",
+                "#c3909d",
+                "#dd6e2e"
+            ]
+        },
+        {
+            bright: true,
+            neutral: true,
+            colors: [
+                "#3c2f3e",
+                "#fbad87",
+                "#ecc7c0",
+                "#8ccac7",
+                "#dae8e3"
+            ]
+        },
+        {
+            muted: true,
+            neutral: true,
+            colors: [
+                "#38334a",
+                "#6a4e6d",
+                "#6980a5",
+                "#757080",
+                "#d0d3d0"
+            ]
+        },
+        {
+            muted: true,
+            neutral: true,
+            colors: [
+                "#697e8d",
+                "#e9e6c2",
+                "#d6d5bd",
+                "#c3a770",
+                "#a74b22"
+            ]
+        },
+        {
+            muted: true,
+            neutral: true,
+            colors: [
+                "#c0c4b6",
+                "#eadfdb",
+                "#aeafb1",
+                "#d8c2b5",
+                "#c18a6a"
+            ]
+        },
+        {
+            muted: true,
+            cool: true,
+            colors: [
+                "#3d3d3b",
+                "#4c586f",
+                "#ebedec",
+                "#a3a9b0",
+                "#ccc4c0"
+            ]
+        },
+        {
+            muted: true,
+            cool: true,
+            colors: [
+                "#8b8589",
+                "#989898",
+                "#838996",
+                "#979aaa",
+                "#4c516d"
+            ]
+        },
+        {
+            muted: true,
+            cool: true,
+            colors: [
+                "#e0ca99",
+                "#aca786",
+                "#918a77",
+                "#626b66",
+                "#434a42"
+            ]
+        },
+        {
+            muted: true,
+            warm: true,
+            colors: [
+                "#f5ded5",
+                "#e8bbac",
+                "#e1af92",
+                "#844e4c",
+                "#462527"
+            ]
+        },
+        {
+            muted: true,
+            warm: true,
+            colors: [
+                "#485664",
+                "#8d7c92",
+                "#cfa6c0",
+                "#1f3231",
+                "#f5c1d0"
+            ]
+        },
+        {
+            muted: true,
+            warm: true,
+            colors: [
+                "#a6a5b5",
+                "#887a89",
+                "#b5a6ab",
+                "#ecd0cf",
+                "#f3f0eb"
+            ]
+        },
+        {
+            clean: true,
+            cool: true,
+            colors: [
+                "#46464e",
+                "#73767d",
+                "#a6cbd4",
+                "#f7f6f2",
+                "#f3f0eb"
+            ]
+        },
+        {
+            clean: true,
+            cool: true,
+            colors: [
+                "#f3e7c1",
+                "#a0c1b8",
+                "#6f9eb2",
+                "#726994",
+                "#331f38"
+            ]
+        },
+        {
+            clean: true,
+            cool: true,
+            colors: [
+                "#557b83",
+                "#39aea9",
+                "#a2d5ab",
+                "#e5efc1",
+                "#5b5151"
+            ]
+        },
+        {
+            clean: true,
+            neutral: true,
+            colors: [
+                "#584848",
+                "#f3b54a",
+                "#d8d9de",
+                "#6e7889",
+                "#393b45"
+            ]
+        },
+        {
+            clean: true,
+            neutral: true,
+            colors: [
+                "#d55c3d",
+                "#677279",
+                "#6a91ab",
+                "#c4d7df",
+                "#f5f5f5"
+            ]
+        },
+        {
+            clean: true,
+            neutral: true,
+            colors: [
+                "#747474",
+                "#8ec3af",
+                "#e66cc5",
+                "#d5d5d5",
+                "#01395e"
+            ]
+        },
+        {
+            clean: true,
+            warm: true,
+            colors: [
+                "#1a3445",
+                "#e7c67f",
+                "#e1b49d",
+                "#99abb9",
+                "#c5d5dd"
+            ]
+        },
+        {
+            clean: true,
+            warm: true,
+            colors: [
+                "#ccaa87",
+                "#c09569",
+                "#abc2db",
+                "#87a9cc",
+                "#e1e1e1"
+            ]
+        },
+        {
+            clean: true,
+            warm: true,
+            colors: [
+                "#f194a3",
+                "#fedad3",
+                "#f0f0f0",
+                "#ccd5d8",
+                "#8a939c"
+            ]
+        },
 
 
 
 
     ]
-
-    animate();
-    
-    function animate() {
-
-        $(".bar").addClass("bar-end");
-    }
-
 
     //  get temperature selection and make new array with just correct temperature
     $(".temp-button").on("click", function () {
@@ -369,7 +668,6 @@ $(function () {
 
     // this defines what the colors are 
     // and adds them to the dom
-
     function defineColors(orderInArray) {
         // color0 = finalArray[orderInArray].colors[0];
         for (let i = 0; i < 3; i = i + 1) {
@@ -383,13 +681,14 @@ $(function () {
         rgb2hex(colorName, colorContainer);
     });
 
+    // to allow color visibility on touch screens
     $(".container").on("touchstart", ".color-container", function () {
         let colorName = $(this).css("background-color");
         let colorContainer = $(this);
         rgb2hex(colorName, colorContainer);
     });
 
-
+    // to convert autimatic rgb color to hex
     function rgb2hex(rgb, currentContainer) {
         rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
         function hex(x) {
